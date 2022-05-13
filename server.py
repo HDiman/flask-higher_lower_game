58ce2faf10d1294760eaccf6998ea7e3c5f01c7c
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-05-13 13:03:13 +0300

    Project is finished

--- server.py
+++ server.py
@@ -1,15 +1,29 @@
 from flask import Flask
+import random
 
+random_number = random.randint(0, 9)
+print(random_number)
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def guess_number():
     return '<h1>Guess a number between 0 and 9</h1>'\
            '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif">'
 
-@app.route("/username/<name>")
-def greet(name):
-    return f"Hello {name}"
+@app.route("/<choice_number>")
+def greet(choice_number):
+    if int(choice_number) < random_number:
+        return '<h1><font color="red">Too low, try again</h1>' \
+               '<img src="https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif">'
+    elif int(choice_number) > random_number:
+        return '<h1><font color="blue">Too high, try again<h1>' \
+               '<img src="https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif">'
+    elif int(choice_number) == random_number:
+        return '<h1><font color="green">You found me!</h1>' \
+               '<img src="https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif">'
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
