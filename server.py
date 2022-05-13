a9aec0452d3391424ebbdfd14b39f9d300215456
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-05-13 13:24:35 +0300

    Project is finished

--- server.py
+++ server.py
@@ -10,15 +10,15 @@ def guess_number():
     return '<h1>Guess a number between 0 and 9</h1>'\
            '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif">'
 
-@app.route("/<choice_number>")
+@app.route("/<int:choice_number>")
 def greet(choice_number):
-    if int(choice_number) < random_number:
+    if choice_number < random_number:
         return '<h1><font color="red">Too low, try again</h1>' \
                '<img src="https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif">'
-    elif int(choice_number) > random_number:
+    elif choice_number > random_number:
         return '<h1><font color="blue">Too high, try again<h1>' \
                '<img src="https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif">'
-    elif int(choice_number) == random_number:
+    elif choice_number == random_number:
         return '<h1><font color="green">You found me!</h1>' \
                '<img src="https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif">'
 
